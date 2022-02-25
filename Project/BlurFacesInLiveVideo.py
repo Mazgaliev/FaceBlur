27ff581520905bca827f73ed545c2fc1c9c1fa59
Mite <mite_mazgaliev@live.com>
2022-02-25 21:28:49 +0100

    Blur Face project

--- Project/BlurFacesInLiveVideo.py
+++ Project/BlurFacesInLiveVideo.py
@@ -2,7 +2,13 @@ import cv2
 import numpy as np
 
 
-#Blur function
+# Small project that uses python open CV to live blur the face of a person
+# It is not very optimised so it might be a bit slow with low fps to achieve the possible blurring.
+# It uses a already trained haar_cascade_FrontalFaceclassificator so it is good at blurring faces
+# looking directly at the camera,# otherwise not so much.
+# Face detection can be optimised using deep learning algorithms.
+
+# Blur function
 def blurFaces(image):
     imig = image
     faces = detectFaces(image)
@@ -13,7 +19,7 @@ def blurFaces(image):
     return img
 
 
-#Detect face function
+# Detect face function
 def detectFaces(image):
     cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
